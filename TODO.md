f6b09c5d943f95d5c2839078f37dae1586985388
Martin Xu <martin.xus@gmail.com>
2022-10-21 21:24:30 +0800

    move_semantics

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 * [x] variables 2022-10-21 20:51:07
 * [x] functions 2022-10-21 20:54:57
-* [ ] move_semantics
+* [x] move_semantics 2022-10-21 21:24:23
 * [ ] primitive_types
 * [ ] structs
 * [ ] enums
--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,23 +1,16 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
-
-    let vec1 = fill_vec(vec0);
-
+    let mut vec1: Vec<i32> = vec![];
+    fill_vec(&mut vec1);
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
-
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) -> &mut Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
